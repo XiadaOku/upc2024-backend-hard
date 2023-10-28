1239aa7f647fd16372f6b81a081007eefd0fb5a5
XiadaOku <69246170+XiadaOku@users.noreply.github.com>
2023-10-29 01:02:13 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   require("dotenv").config();
   
   const app = await NestFactory.create(AppModule);
+
+  app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'http://upc2024-backend-hard.onrender.com', 'https://upc2024-backend-hard.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
