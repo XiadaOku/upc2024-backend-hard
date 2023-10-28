9f2fed018e62fc770eebe423f5b5336e97147bb6
XiadaOku <69246170+XiadaOku@users.noreply.github.com>
2023-10-29 00:52:04 +0300

    maybe that way

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
 
 async function bootstrap() {
   require("dotenv").config();
   
   const app = await NestFactory.create(AppModule);
-  
-  app.use(
-    '/graphql',
-    cors<cors.CorsRequest>({ origin: ['http://localhost', 'https://upc2024-backend-hard.onrender.com'] }),
-  );
-
   await app.listen(3000);
 }
 bootstrap();
