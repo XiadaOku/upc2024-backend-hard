148ebb6c161264da5c4135d8a5d58a8357714dc3
XiadaOku <69246170+XiadaOku@users.noreply.github.com>
2023-10-29 00:37:35 +0300

    more cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   require("dotenv").config();
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
