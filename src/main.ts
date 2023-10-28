7f8a9bc8a52b817349cc30019dbd8c6b55551be2
XiadaOku <69246170+XiadaOku@users.noreply.github.com>
2023-10-29 00:45:32 +0300

    and another one

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   require("dotenv").config();
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>({ origin: ['http://localhost', 'https://upc2024-backend-hard.onrender.com'] }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
